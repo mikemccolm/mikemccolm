4d1bd434f5a6271ae95f1a31cd04416f3da8f639
mikemccolm <50307577+mikemccolm@users.noreply.github.com>
2023-01-17 21:25:15 -0500

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -54,6 +54,7 @@ function getteams()
     fetch("http://127.0.0.1:8080/teams", requestOptions)
     .then(response => response.json())
     .then(result => updateTeams(result.teams))
+    .catch(err => console.log('An Error Happened, API is likely unavailable'+ err.message))
 }
 
 function updateTeams(teams)
